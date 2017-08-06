cd58afd5b8c2706e952a441228ed5d426c6c7162
DefaltSimon <simon.peter.goricar@gmail.com>
2017-08-06 11:23:21 +0200

    Updated example to include all info

--- examples/user.py
+++ examples/user.py
@@ -2,19 +2,44 @@
 import asyncio
 import osu_ds
 
+import time
+import configparser
+
+parser = configparser.ConfigParser()
+parser.read("config.ini")
+
 # You can get your api key from https://osu.ppy.sh/p/api
-key = "api key here"
+key = parser.get("osu", "key")
 
 osu = osu_ds.OsuApi(api_key=key)
 loop = asyncio.get_event_loop()
 
 async def show_user_info(name):
+    t = time.clock()
+
     ds = await osu.get_user(name)
 
-    print(ds.name,
-          ds.level,
-          ds.playcount,
-          ds.country_rank,
-          ds.avatar_url)
+    print("Search took {}".format(time.clock() - t))
 
-loop.run_until_complete(show_user_info("DefaltSimon"))
+    s = """Name: {} (id:{})
+Level: {}
+Playcount: {}
+Accuracy: {} %
+Score: {} (ranked), {} (total)
+PP: {}
+Rank: {} (world), {} (country)
+Amounts: {} (ss), {} (s), {} (a)
+Country: {}
+
+Urls:
+    {} (profile)
+    {} (avatar)
+""".format(ds.name, ds.id, ds.level, ds.playcount, ds.accuracy,
+           ds.ranked_score, ds.total_score, ds.pp, ds.world_rank, ds.country_rank,
+           ds.ss_amount, ds.s_amount, ds.a_amount, ds.country, ds.profile_url, ds.avatar_url)
+
+    print(s)
+
+while True:
+    name = input(">")
+    loop.run_until_complete(show_user_info(name))
